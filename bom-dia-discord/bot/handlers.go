8c47290bf1e9a64cc8040e86e2207d5997aba246
Caio Vieira <caiocesarav0208@gmail.com>
2025-01-23 00:38:07 +0000

    feat: :sparkles: adiciona comando para retornar o ponto
    tambem adiciona timezone

--- bom-dia-discord/bot/handlers.go
+++ bom-dia-discord/bot/handlers.go
@@ -46,6 +46,24 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
+	if m.GuildID == "" {
+		//mensagem enviada em DM
+		handleDM(s, m)
+	} else {
+		//mensagem enviada no canal
+		handleChannelMessages(s, m)
+	}
+
+}
+
+func handleChannelMessages(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Carregar a localização "America/Sao_Paulo"
+	location, err := time.LoadLocation("America/Sao_Paulo")
+	if err != nil {
+		fmt.Println("Erro ao carregar fuso horário:", err)
+		return
+	}
+
 	// Responder a mensagens específicas
 	switch m.Content {
 	case "bom dia":
@@ -55,7 +73,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		mockBanco = append(mockBanco, Banco{
 			nome:        nome,
 			id:          id,
-			goodMorning: time.Now(),
+			goodMorning: time.Now().In(location),
 		})
 
 		fmt.Print(mockBanco)
@@ -70,7 +88,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		found := false
 		for index, banco := range mockBanco {
 			if banco.id == id && !banco.goodMorning.IsZero() && banco.closing.IsZero() {
-				mockBanco[index].closing = time.Now()
+				mockBanco[index].closing = time.Now().In(location)
 				found = true
 				break
 			}
@@ -92,4 +110,62 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	default:
 		fmt.Println("Comando não reconhecido.")
 	}
+}
+
+func handleDM(s *discordgo.Session, m *discordgo.MessageCreate) {
+	switch m.Content {
+	case "!ponto":
+		// Carregar a localização "America/Sao_Paulo"
+		location, err := time.LoadLocation("America/Sao_Paulo")
+		if err != nil {
+			fmt.Println("Erro ao carregar fuso horário:", err)
+			return
+		}
+
+		id := m.Author.ID
+		tabela := "💾 **Tabela de Registros Ponto**\n```\n"                                                  // Começa como um bloco de código
+		tabela += fmt.Sprintf("%-20s | %-10s | %-10s | %-10s \n", "Nome", "Bom dia", "Encerrando", "Total") // Cabeçalho formatado
+		tabela += fmt.Sprintf("%s\n", "---------------------+------------+------------+------------")       // Separador
+
+		// Encontrar e editar a entrada no mockBanco com o mesmo id
+		found := false
+		for index, banco := range mockBanco {
+			if banco.id == id {
+				row := mockBanco[index]
+				goodMorning := row.goodMorning
+				closing := row.closing
+
+				diff := closing.Sub(goodMorning)
+
+				tabela += fmt.Sprintf("%-20s | %02d:%02d:%02d   | %02d:%02d:%02d   | ",
+					row.nome,
+					goodMorning.In(location).Hour(),
+					goodMorning.In(location).Minute(),
+					goodMorning.In(location).Second(),
+					closing.In(location).Hour(),
+					closing.In(location).Minute(),
+					closing.In(location).Second(),
+				)
+
+				if closing.IsZero() {
+					tabela += "-\n"
+				} else {
+					tabela += fmt.Sprintf("%v\n",
+						diff)
+				}
+				found = true
+			}
+		}
+
+		tabela += "```"
+
+		if found {
+			s.ChannelMessageSend(m.ChannelID, tabela)
+		} else {
+
+		}
+	default:
+		fmt.Println("Comando não reconhecido.")
+	}
+
 }
